9111282455e09eb2924afb2cf662231d2ca4f5cf
Miguel Botelho <m_botelho8@hotmail.com>
2016-03-24 19:23:15 +0000

    It stores the input and output of content.

---
+++ SetDSLtoJava/src/grammar/Attribute.java
@@ -0,0 +1,57 @@
+package grammar;
+
+import java.util.ArrayList;
+
+public class Attribute {
+	private String variable;
+	private Integer size;
+	private Boolean type; // true é INPUT, false é OUTPUT
+	private ArrayList<Integer> content;
+
+	public Attribute(String variable, Integer size, Boolean type, ArrayList<Integer> content) {
+		this.variable = variable;
+		this.size = size;
+		this.type = type;
+		this.content = content;
+	}
+
+	public String getVariable() {
+		return variable;
+	}
+
+	public Integer getSize(){
+		return size;
+	}
+
+	public Boolean getType() {
+		return type;
+	}
+	
+	public ArrayList<Integer> getContent() {
+		return content;
+	}
+	
+	public void setContent(ArrayList<Integer> content) {
+		this.content = content;
+	}
+
+	public void setVariable(String variable) {
+		this.variable = variable;
+	}
+
+	public void setSize(Integer size) {
+	  	this.size = size;
+	}
+
+	public void setType(Boolean type){
+		this.type = type;
+	}
+
+	public boolean equals(Attribute attr) {
+        return this.getVariable() == attr.getVariable();
+    }
+	
+	public String toString() {
+		return "Content: " + content.toString() + " ,Size: " + size + "\n";
+	}
+}
